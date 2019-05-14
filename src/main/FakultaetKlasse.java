b55a1140d39967f3de5acabdc90d8ee3a331aa4a
je-99 <ja.ehr99@gmail.com>
2019-05-14 20:38:20 +0200

    Kommentare

--- src/main/FakultaetKlasse.java
+++ src/main/FakultaetKlasse.java
@@ -1,7 +1,14 @@
 package main;
 
-public class FakultaetKlasse {
-
+/**
+ * Berchnung der Fakultaet als Top-Level-Klasse
+ */
+public class FakultaetKlasse{
+    /**
+     * Berechnung der Fakultaet
+     * @param x wert
+     * @return Fakultaet
+     */
     public static int FakultaetBerechnen(int x) {
         int ergebnis = 1;
         for (int i = 1; i <= x; i++) {
